5180523adf1c54fa9cc7b0b7d4a6bbab7c120456
Albertas Grigoryan <albertasgrigoryan@gmail.com>
2023-05-17 19:14:56 +0300

    console kernel

---
+++ corum/source/console/Kernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace allbertss\psittacorum\console;
+
+class Kernel
+{
+    /*
+     * Console's statuses
+     * 0 - The application completed successfully.
+     * 1 - The application encountered an error or invalid input.
+     * 2 - The application was unable to find the specified file.
+     * 3 - The application encountered a runtime error.
+     * 4 - The application ran out of memory.
+     * 5 - The user cancelled the application.
+     * 6 - The application attempted to access a resource it was not authorized to use.
+     * 7 - The application encountered an internal error or crash.
+     * 8 - The application encountered a network error.
+     * 9 - The application was terminated by a signal.
+     */
+    public function handle(): int
+    {
+        return 0;
+    }
+}
