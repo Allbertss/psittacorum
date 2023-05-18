29328add656f4e68c6b136a5aebd7af09b1180ee
Albertas Grigoryan <albertasgrigoryan@gmail.com>
2023-05-18 21:58:48 +0300

    user entity created

---
+++ source/entity/User.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\entity;
+
+class User
+{
+    public function __construct(
+        private ?int               $id,
+        private string             $username,
+        private string             $password,
+        private \DateTimeImmutable $createdAt
+    )
+    {
+    }
+
+    public static function create(string $username, string $password): self
+    {
+        return new self(
+            null,
+            $username,
+            password_hash($password),
+            new \DateTimeImmutable()
+        );
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return User
+     */
+    public function setId(?int $id): User
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param string $username
+     * @return User
+     */
+    public function setUsername(string $username): User
+    {
+        $this->username = $username;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     * @return User
+     */
+    public function setPassword(string $password): User
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $createdAt
+     * @return User
+     */
+    public function setCreatedAt(\DateTimeImmutable $createdAt): User
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
+}
