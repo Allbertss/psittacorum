232c315d2f72bfee39e0ef0e233e338129f3ac12
Albertas Grigoryan <albertasgrigoryan@gmail.com>
2023-05-18 19:13:51 +0300

    getters & setters for home entity

--- source/entity/Home.php
+++ source/entity/Home.php
@@ -27,4 +27,76 @@ class Home
             $createdAt ?? new \DateTimeImmutable()
         );
     }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     * @return Home
+     */
+    public function setId(?int $id): Home
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return Home
+     */
+    public function setTitle(string $title): Home
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param string $body
+     * @return Home
+     */
+    public function setBody(string $body): Home
+    {
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @return \DateTimeImmutable
+     */
+    public function getCreatedAt(): \DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param \DateTimeImmutable $createdAt
+     * @return Home
+     */
+    public function setCreatedAt(\DateTimeImmutable $createdAt): Home
+    {
+        $this->createdAt = $createdAt;
+        return $this;
+    }
 }
